18e7ddaa9d1b208103445ade55c6ff25a553b1d4
Abdulrahman <AbdulrahmanAlhatti@gmail.com>
2022-11-20 23:26:30 +0300

    used while loop and give some enhancement

--- CoffeeCliApp/Program.cs
+++ CoffeeCliApp/Program.cs
@@ -16,51 +16,38 @@ namespace CoffeeCliApp
             Console.WriteLine("Small\t\t$3");
             Console.WriteLine("Medium\t\t$5");
             Console.WriteLine("Large\t\t$7");
+            Console.WriteLine("Type exit to exit");
 
+            int total = 0;
             String input = Console.ReadLine();
+            while (input != "exit")
+            {
+                switch (input)
+                {
+                    case "Small":
+                        Console.WriteLine("You orderd a Small Coffee");
+                        total += 3;
+                        break;
 
-            switch (input)
-            {
-                case "Small":
-                   Console.WriteLine("You orderd a Small Coffee");
-                   Console.WriteLine("Total: $3");
-                    break;
+                    case "Medium":
+                        Console.WriteLine("You orderd a Medium Coffee");
+                        total += 5;
+                        break;
 
-                case "Medium":
-                    Console.WriteLine("You orderd a Medium Coffee");
-                    Console.WriteLine("Tottal: $5");
-                    break;
+                    case "Large":
+                        Console.WriteLine("You orderd a Medium Coffee");
+                        total += 7;
+                        break;
 
-                case "Large":
-                    Console.WriteLine("You orderd a Medium Coffee");
-                    Console.WriteLine("Tottal: $5");
-                    break;
-
-                default:
-                    Console.WriteLine("Your choice is unavailbale");
-                    break;
+                    default:
+                        Console.WriteLine("Your choice is unavailbale");
+                        break;
+                }
+                Console.WriteLine("Total: $"+ total);
+                Console.WriteLine("Do you want anything else?");
+                input = Console.ReadLine();
             }
-            /*
-            if (input == "Small")
-            {
-                Console.WriteLine("You orderd a Small Coffee");
-                Console.WriteLine("Total: $3");
-            }
-            else if (input == "Medium")
-            {
-                Console.WriteLine("You orderd a Medium Coffee");
-                Console.WriteLine("Tottal: $5");
-            }
-            else if (input == "Large")
-            {
-                Console.WriteLine("You orderd a Large Coffee");
-                Console.WriteLine("Total: $7");
-            }
-            else
-            {
-                Console.WriteLine("Your choice is unavailbale");
-            }
-            */
+            Console.WriteLine("Thank you for your shopping");
             Console.ReadKey();
 
         }
